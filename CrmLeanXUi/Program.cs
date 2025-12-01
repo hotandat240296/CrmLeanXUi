f146be546d6ea46f213ef039855621e3b94c33e1
hotandat240296 <62368505+hotandat240296@users.noreply.github.com>
2025-12-01 10:31:11 +0700

    [Copy code old]

--- CrmLeanXUi/Program.cs
+++ CrmLeanXUi/Program.cs
@@ -1,25 +1,32 @@
 using CrmLeanXUi.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents();
+// Razor Components + Interactive Server
+builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+// MudBlazor
+builder.Services.AddMudServices();
+
+// Authorization (optional)
+builder.Services.AddAuthorizationCore();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>();
+// Map root component
+app.MapRazorComponents<App>()
+   .AddInteractiveServerRenderMode();
 
 app.Run();
